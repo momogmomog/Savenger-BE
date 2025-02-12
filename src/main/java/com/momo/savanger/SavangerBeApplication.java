34e4ffadfb326e9fc443c5e9225f363e8d1454df
Mihaela Georgieva <momog@abv.bg>
2025-02-12 14:21:05 +0200

    Initial commit!

---
+++ src/main/java/com/momo/savanger/SavangerBeApplication.java
@@ -0,0 +1,13 @@
+package com.momo.savanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavangerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavangerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momo/savanger/SavangerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momo.savanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavangerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
