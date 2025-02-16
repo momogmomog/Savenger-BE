2e60abeedfe7bbafc783091c15ee67ff0e0c40f7
Tsvetelin Yakimov <ceci2205@abv.bg>
2025-02-16 22:36:07 +0200

    Add Google Style.

--- src/test/java/com/momo/savanger/SavangerBeApplicationTests.java
+++ src/test/java/com/momo/savanger/SavangerBeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SavangerBeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
