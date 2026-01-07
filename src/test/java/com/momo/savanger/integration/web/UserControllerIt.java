6025a09d7a06f96bef4e6df933d30f9367ff1983
Mihaela Georgieva <momog@abv.bg>
2026-01-07 22:51:12 +0200

    Add json data check.

--- src/test/java/com/momo/savanger/integration/web/UserControllerIt.java
+++ src/test/java/com/momo/savanger/integration/web/UserControllerIt.java
@@ -1,8 +1,15 @@
 package com.momo.savanger.integration.web;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.momo.savanger.api.user.User;
+import com.momo.savanger.api.user.UserRepository;
 import com.momo.savanger.constants.Endpoints;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -17,36 +24,48 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @Sql(value = "classpath:/sql/del-user-it-data.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 public class UserControllerIt extends BaseControllerIt {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     @WithLocalMockedUser(username = Constants.FIRST_USER_USERNAME)
     public void testGetDetails() throws Exception {
 
-        this.getOK(Endpoints.USER_DETAILS);
+       User user =  this.userRepository.findById(1L).orElse(null);
+        System.out.println(user.getDateRegistered());
+
+        super.getOK(Endpoints.USER_DETAILS,
+                jsonPath("$.id", is(1)),
+                jsonPath("$.username", is("Ignat"))
+        );
     }
 
     @Test
     public void testGetDetails_withoutMockedUser() throws Exception {
 
-        this.get(Endpoints.USER_DETAILS, HttpStatus.UNAUTHORIZED);
+        super.get(Endpoints.USER_DETAILS, HttpStatus.UNAUTHORIZED);
     }
 
     @Test
     @WithLocalMockedUser(username = Constants.FIRST_USER_USERNAME)
     public void testGetOtherUser() throws Exception {
 
-        this.getOK("/user-details/Coco");
+        super.getOK("/user-details/Coco",
+                jsonPath("$.id", is(3)),
+                jsonPath("$.username", is("Coco"))
+        );
     }
 
     @Test
     @WithLocalMockedUser(username = Constants.FIRST_USER_USERNAME)
     public void testGetOtherUser_invalidUsername() throws Exception {
 
-        this.get("/user-details/momo", HttpStatus.NOT_FOUND);
+        super.get("/user-details/momo", HttpStatus.NOT_FOUND);
     }
 
     @Test
     public void testGetOtherUser_withNoMockedUser() throws Exception {
 
-        this.get("/user-details/Coco", HttpStatus.UNAUTHORIZED);
+        super.get("/user-details/Coco", HttpStatus.UNAUTHORIZED);
     }
 }
