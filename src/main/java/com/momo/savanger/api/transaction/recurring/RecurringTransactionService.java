10b3a1e5081ea6fab15716de38c1d5311dcffe3b
Tsvetelin Nikolaev Yakimov <ceci2205@abv.bg>
2026-02-22 23:25:18 +0200

    Add endpoints for retrieving and updating recurring transactions. (#48)

--- src/main/java/com/momo/savanger/api/transaction/recurring/RecurringTransactionService.java
+++ src/main/java/com/momo/savanger/api/transaction/recurring/RecurringTransactionService.java
@@ -7,6 +7,8 @@ public interface RecurringTransactionService {
 
     RecurringTransaction create(CreateRecurringTransactionDto dto);
 
+    RecurringTransaction edit(Long recurringTransactionId, CreateRecurringTransactionDto dto);
+
     RecurringTransaction findById(Long id);
 
     RecurringTransaction findByIdFetchAll(Long id);
